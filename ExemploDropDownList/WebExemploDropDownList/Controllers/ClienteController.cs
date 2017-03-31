ac92695af8e7e0ee054e02ef8c506104724edfe2
eli <elipedrodos.santos@gmail.com>
2017-03-31 20:05:17 -0300

    OK

---
+++ ExemploDropDownList/WebExemploDropDownList/Controllers/ClienteController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using WebExemploDropDownList.Models;
+
+namespace WebExemploDropDownList.Controllers
+{
+    public class ClienteController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.ClienteId = new SelectList
+                                (
+                                    new Cliente().ListaClientes(),
+                                    "ClienteId", 
+                                    "Nome"
+                                );
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string clienteId)
+        {
+            ViewBag.ClienteId = new SelectList
+                                (
+                                    new Cliente().ListaClientes(),
+                                    "ClienteId",
+                                    "Nome",
+                                    clienteId
+                                );
+
+
+            return View();
+        }
+    }
+}
---
+++ ExemploDropDownList/WebExemploDropDownList/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace WebExemploDropDownList.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+
+        public List<Cliente> ListaClientes()
+        {
+            return new List<Cliente>
+            {
+                new Cliente {ClienteId = 0, Nome = "Pedro" },
+                new Cliente {ClienteId = 1, Nome = "Karol" },
+                new Cliente {ClienteId = 2, Nome = "Maria" },
+                new Cliente {ClienteId = 3, Nome = "Sandra" },
+            };            
+        }
+    }
+}
